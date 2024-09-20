dda48bcfb33dd8c41ababdc8d3aca6942b27c05e
Awus Abuaisha <awusabuaisha@gmail.com>
2024-09-20 18:47:30 +0100

    trying to get things working

--- src/script.js
+++ src/script.js
@@ -5,15 +5,15 @@ import {
 const init = async () => {
     try {
         const user = await checkAuthState();
-        if (!window.location.pathname.endsWith('index.html')) {
-            window.location.href = 'index.html';
+        if (!window.location.pathname.endsWith('')) {
+            window.location.href = '/';
         }
         console.log('User is already logged in, redirecting to index page');
 
     } catch (error) {
         console.log('User is not logged in');
-        if (window.location.pathname.endsWith('index.html')) {
-            window.location.href = 'Login.html';
+        if (window.location.pathname.endsWith('/')) {
+            window.location.href = '/src/Login.html';
         }
     }
 };
